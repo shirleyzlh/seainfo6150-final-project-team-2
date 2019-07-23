d0b801b44a0600ada49e99ae3403fdb9d34c3f55
shirleyzlhh <shirleyzlh@gmail.com>
2019-07-23 13:13:51 -0700

    schools

--- src/Schools/Schools.jsx
+++ src/Schools/Schools.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { NavLink } from "react-router-dom";
 
 import Degrees from "../Degrees/Degrees";
+import 'antd/dist/antd.css';
+import { Menu, Dropdown, Icon } from 'antd';
+import MenuItem from "antd/lib/menu/MenuItem";
+
+import styles from "./Schools.module.css";
 
 const Schools = ({ degrees, schools }) => {
   return (
@@ -13,10 +19,26 @@ const Schools = ({ degrees, schools }) => {
         const thisSchoolsDegrees = degrees.filter(degree =>
           degree.schools.includes(school.slug)
         );
+        const degreesMenu = (
+          <Menu mode="vertical">
+            {thisSchoolsDegrees.map((degree, index) => {
+              return <MenuItem><NavLink to={'/degree/' + degree.slug}>{degree.title}</NavLink></MenuItem>
+            })}
+          </Menu>
+        );
 
         return (
-          <div key={`${school.slug}`}>
-            Schools display attributes
+          <div key={`${school.slug}`} className={styles.schoolItem}>
+            <header>
+              <h1>School of {school.name}</h1>
+              <p>{school.short_summary}</p>
+              <Dropdown overlay={degreesMenu}>
+                <a className="ant-dropdown-link" href="#">
+                  Degrees <Icon type="down" />
+                </a>
+              </Dropdown>
+            </header>
+            <img src={school.image} alt={school.title} />
           </div>
         );
       })}
--- src/SchoolsPage/SchoolsPage.jsx
+++ src/SchoolsPage/SchoolsPage.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Schools from "../Schools/Schools";
 
+import styles from "./SchoolsPage.module.css";
+
 const SchoolsPage = ({ degrees, schools }) => {
   return (
-    <div>
-      This is the Schools page 
+    <div className={styles.allPage}>
+      <div className={styles.title}>Schools and Colleges</div>
       <hr />
       <Schools degrees={degrees} schools={schools} />
     </div>
